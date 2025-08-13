ec6ebd029874ca0d76af1d52857d6dbbabbafe5c
JRosadoDiaz <jose.rosado853@gmaill.com>
2025-08-13 14:41:49 -0600

    Started building web server, pinger mostly complete

---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+/*
+This is the entry point for the program.
+It should contain the main function, where you'll parse command-line flags, set up your services, and start the application.
+This file should be kept as clean as possible, primarily acting as a coordinator.
+*/
+
+// go run AT_Skill_Assessment/cmd/main.go www.google.com,www.reddit.com world 3
+
+import (
+	"flag" // Parses command line arguments
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/JRosadoDiaz/AT_Skill_Assessment/internal/pinger"
+	"github.com/JRosadoDiaz/AT_Skill_Assessment/web"
+)
+
+// Command-line flags
+var hostsStr string
+var port string
+var interval time.Duration
+var count int
+
+func main() {
+	// Grabbing flags
+	flag.StringVar(&hostsStr, "hosts", "", "comma-seperated list of hosts to ping")
+	flag.StringVar(&port, "port", "8000", "Port number")
+	flag.DurationVar(&interval, "interval", time.Second*5, "The interval between pings")
+	flag.IntVar(&count, "count", 5, "Number of times the host will be pinged")
+	flag.Parse()
+
+	if hostsStr == "" {
+		fmt.Printf("Error: The 'hosts' flag is required.\nDid you forget to add '--hosts='?\n")
+		flag.Usage()
+		return
+	}
+	hosts := strings.Split(hostsStr, ",")
+
+	// Generate ping manager
+	var pingStruct = pinger.NewPingerManager(hosts, interval)
+
+	// Start web server
+	web.StartServer(port, pingStruct)
+
+	fmt.Println("Starting pinger")
+	pingStruct.StartPinging()
+	select {} // Will keep the project running indefinitely
+}
--- go.mod
+++ go.mod
@@ -2,9 +2,12 @@ module github.com/JRosadoDiaz/AT_Skill_Assessment
 
 go 1.24.6
 
+require github.com/prometheus-community/pro-bing v0.7.0
+
 require (
+	github.com/go-chi/chi v1.5.5
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/prometheus-community/pro-bing v0.7.0 // indirect
+	github.com/gorilla/websocket v1.5.3
 	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
