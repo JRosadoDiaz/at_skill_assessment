2d561a09212071a6618bcc73ff1e2b25d7406c87
JRosadoDiaz <jose.rosado853@gmaill.com>
2025-08-13 15:16:23 -0600

    Web server can run the index html

--- web/server.go
+++ web/server.go
@@ -2,8 +2,11 @@ package web
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
+	"github.com/JRosadoDiaz/AT_Skill_Assessment/api"
 	"github.com/JRosadoDiaz/AT_Skill_Assessment/internal/pinger"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -17,17 +20,27 @@ func StartServer(port string, pm *pinger.PingManager) {
 	router := chi.NewRouter() // Returns a new Mux object that implements the Router interface
 	router.Use(middleware.Logger)
 
-	router.Get("/hello", basicHandler)
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("./web/templates/index.html")
+		if err != nil {
+			api.InternalErrorHandler(w)
+			log.Printf("Error loading template: %v", err)
+			return
+		}
 
-	server := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
-	}
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			api.InternalErrorHandler(w)
+			log.Printf("Error rendering template: %v", err)
+			return
+		}
+	})
 
 	fmt.Printf("Server is now listening at http://localhost:%v \n", port)
-	err := server.ListenAndServe()
+	err := http.ListenAndServe(":"+port, router)
 	if err != nil {
 		fmt.Println("failed to listen to server", err)
+		log.Printf("Failed to listen to server: %v", err)
 	}
 }
 
@@ -40,7 +53,7 @@ func StartServer(port string, pm *pinger.PingManager) {
 
 // }
 
-// func Handler(r *chi.Mux, pinger *pinger.PingManager) {
+// func handler(r *chi.Mux, pinger *pinger.PingManager) { // Mux -> Multiplexer
 // 	r.Route("/metrics", func(router chi.Router) {
 // 		router.Get("/", pinger.GetMetrics)
 // 	})
